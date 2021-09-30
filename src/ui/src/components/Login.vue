f16dad3ea2724c5fc4e035d2eb19db9eb34477aa
Lee Collins <javaConductor@yahoo.com>
2021-09-29 23:07:38 -0500

    again

--- src/ui/src/components/Login.vue
+++ src/ui/src/components/Login.vue
@@ -1,61 +1,67 @@
 <template>
-  <div class="login">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
+  <v-container class="login">
 
+    <v-row justify="space-around">
 
+      <v-col lg="3" md="3">
+        <v-text-field class="text-field" label="Username" v-model="username"/>
+      </v-col>
+      <v-col lg="1" md="4">
+        <v-text-field class="text-field" label="Password" v-model="password"/>
+      </v-col>
+      <v-col lg="1" md="3">
+        <v-btn @click="onLogin">Login</v-btn>
+      </v-col>
 
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    </v-row>
+
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import backend from '../backend';
+
+  export default {
+    name: 'Login',
+    data: function () {
+      return {
+        // username : "",
+        // password: ""
+      }
+    },
+    props: {
+      msg: String,
+      username: String,
+      password: String,
+      authenticated: Boolean
+    },
+    methods: {
+      async onLogin() {
+        const [u, p] = [this.$props.username, this.$props.password];
+        console.log(`onLogin: user:[${u}]  pswd:[${p}]`);
+        const [, err] = await backend.login(u, p);
+        if (err) {
+          alert(err);
+        }
+      },
+      mounted: function () {
+        console.log(`mounted: user:[${this.$props.username}]  pswd:[${this.$props.password}]`);
+      }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+  .text-field {
+    display: inline-block;
+    margin: 0 10px;
+    width: 100px;
+    height: 20px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
