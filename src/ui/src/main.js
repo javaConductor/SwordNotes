6d6bfdab3d1faf388857cb332a2aea81c6eba722
Lee Collins <javaConductor@yahoo.com>
2021-09-29 23:08:44 -0500

    auth but no action

--- src/ui/src/main.js
+++ src/ui/src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+
+import AsyncComputed from 'vue-async-computed'
+import JsonEditor from 'vue-json-edit'
+
+
+Vue.use(AsyncComputed);
+Vue.use(JsonEditor);
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false
 
